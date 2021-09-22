f464c83161e2f33e8be470294bb01ef94b92055e
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2021-09-22 20:12:36 +0300

    0.1

---
+++ app/src/main/java/com/robik/androidlearning/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.robik.androidlearning
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        val result = findViewById<TextView>(R.id.result)
+        val input = findViewById<EditText>(R.id.input)
+        val radioGroup = findViewById<RadioGroup>(R.id.radio)
+        btn.setOnClickListener {
+            result.text = "${input.text} - ${
+                findViewById<RadioButton>(radioGroup.checkedRadioButtonId).text
+            }"
+        }
+    }
+}
