6a47a0c774fe6f1384c0800eb449dba5aad67d7f
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2021-09-23 15:01:37 +0300

    0.13

--- app/src/main/java/com/robik/androidlearning/MainActivity.kt
+++ app/src/main/java/com/robik/androidlearning/MainActivity.kt
@@ -6,20 +6,28 @@ import android.os.Bundle
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    //@SuppressLint("SetTextI18n")
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val btn = findViewById<Button>(R.id.button)
         val result = findViewById<TextView>(R.id.result)
         val input = findViewById<EditText>(R.id.input)
         val radioGroup = findViewById<RadioGroup>(R.id.radio)
+
         btn.setOnClickListener {
+            val id = radioGroup.checkedRadioButtonId
+
             if(input.text.isBlank()){
-                Toast.makeText(this, "Введи запитання!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Заповни питання!", Toast.LENGTH_SHORT).show()
+            }
+            else if(id == -1){
+                Toast.makeText(this, "Вибери відповідь!", Toast.LENGTH_SHORT).show()
             }
             else result.text = "${input.text} - ${
-                findViewById<RadioButton>(radioGroup.checkedRadioButtonId).text
+                findViewById<RadioButton>(id).text
             }"
         }
     }
